d68e9f46260eaddae9c71e7712dff422543661f0
John Cupitt <jcupitt@gmail.com>
2018-10-28 19:40:23 +0000

    rework webp write
    should be ready for anim to go in

--- libvips/foreign/vips2webp.c
+++ libvips/foreign/vips2webp.c
@@ -58,9 +58,39 @@
 #include "pforeign.h"
 
 #include <webp/encode.h>
+#ifdef HAVE_LIBWEBPMUX
+#include <webp/mux.h>
+#endif /*HAVE_LIBWEBPMUX*/
 
 typedef int (*webp_import)( WebPPicture *picture,
 	const uint8_t *rgb, int stride );
+
+typedef struct {
+	VipsImage *image;
+
+	int Q;
+	gboolean lossless;
+	VipsForeignWebpPreset preset;
+	gboolean smart_subsample;
+	gboolean near_lossless;
+	int alpha_q;
+	gboolean strip;
+
+	WebPConfig config;
+
+	/* We can only really support memory write, thanks to the current webp
+	 * API. When we add metadata, we may need to go back and rewrite the 
+	 * header.
+	 */
+
+	uint8_t *mem;
+
+	/* We want to be able to detect >4gb even on machines that have size_t
+	 * as uint32.
+	 */
+	guint64 size;
+	guint64 max_size;
+} VipsWebPWrite;
 
 static WebPPreset
 get_preset( VipsForeignWebpPreset preset )
@@ -88,37 +118,95 @@ get_preset( VipsForeignWebpPreset preset )
 	return( -1 );
 }
 
-typedef struct {
-	uint8_t *mem;
-
-	/* We want to be able to detect >4gb even on machines that have size_t
-	 * as uint32.
-	 */
-	guint64 size;
-	guint64 max_size;
-} VipsWebPWriter;
-
 static void
-vips_webp_writer_init( VipsWebPWriter *writer ) 
+vips_webp_write_unset( VipsWebPWrite *write )
 {
-	writer->mem = NULL;
-	writer->size = 0;
-	writer->max_size = 0;
+	VIPS_FREE( write->mem );
+}
+
+static int
+vips_webp_write_init( VipsWebPWrite *write, VipsImage *image,
+	int Q, gboolean lossless, VipsForeignWebpPreset preset,
+	gboolean smart_subsample, gboolean near_lossless,
+	int alpha_q, gboolean strip )
+{
+	write->image = image;
+
+	write->Q = Q;
+	write->lossless = lossless;
+	write->preset = preset;
+	write->smart_subsample = smart_subsample;
+	write->near_lossless = near_lossless;
+	write->alpha_q = alpha_q;
+	write->strip = strip;
+
+	write->mem = NULL;
+	write->size = 0;
+	write->max_size = 0;
+
+	if( !WebPConfigInit( &write->config ) ) {
+		vips_webp_write_unset( write );
+		vips_error( "vips2webp",
+			"%s", _( "config version error" ) );
+		return( -1 );
+	}
+
+	/* These presets are only for lossy compression. There seems to be
+	 * separate API for lossless or near-lossless, see
+	 * WebPConfigLosslessPreset().
+	 */
+	if( !(lossless || near_lossless) &&
+		!WebPConfigPreset( &write->config, get_preset( preset ), Q ) ) {
+		vips_webp_write_unset( write );
+		vips_error( "vips2webp", "%s", _( "config version error" ) );
+		return( -1 );
+	}
+
+#if WEBP_ENCODER_ABI_VERSION >= 0x0100
+	write->config.lossless = lossless || near_lossless;
+	write->config.alpha_quality = alpha_q;
+#else
+	if( lossless || 
+		near_lossless )
+		g_warning( "%s", _( "lossless unsupported" ) );
+	if( alpha_q != 100 )
+		g_warning( "%s", _( "alpha_q unsupported" ) );
+#endif
+
+#if WEBP_ENCODER_ABI_VERSION >= 0x0209
+	if( near_lossless )
+		write->config.near_lossless = Q;
+	if( smart_subsample )
+		write->config.preprocessing |= 4;
+#else
+	if( near_lossless )
+		g_warning( "%s", _( "near_lossless unsupported" ) );
+	if( smart_subsample )
+		g_warning( "%s", _( "smart_subsample unsupported" ) );
+#endif
+
+	if( !WebPValidateConfig( &write->config ) ) {
+		vips_webp_write_unset( write );
+		vips_error( "vips2webp", "%s", _( "invalid configuration" ) );
+		return( -1 );
+	}
+
+	return( 0 );
 }
 
 static gboolean
-vips_webp_writer_append( VipsWebPWriter *writer, 
+vips_webp_write_append( VipsWebPWrite *write, 
 	const uint8_t *data, guint64 data_size )
 {
 	guint64 next_size;
 
-	next_size = writer->size + data_size;
+	next_size = write->size + data_size;
 
-	if( next_size > writer->max_size ) {
+	if( next_size > write->max_size ) {
 		uint8_t *new_mem;
 		const guint64 next_max_size =
 			VIPS_MAX( 8192, VIPS_MAX( next_size, 
-				writer->max_size * 2 ) );
+				write->max_size * 2 ) );
 
 		/* We should let it creep up to 4gb rather than just
 		 * blocking when max goes over, but no one will make a >2gb
@@ -131,18 +219,18 @@ vips_webp_writer_append( VipsWebPWriter *writer,
 		}
 
 		if( !(new_mem = (uint8_t *) 
-			g_try_realloc( writer->mem, next_max_size )) ) {
+			g_try_realloc( write->mem, next_max_size )) ) {
 			vips_error( "webp", "%s", _( "out of memory" ) );
 			return( 0 );
 		}
 
-		writer->mem = new_mem;
-		writer->max_size = next_max_size;
+		write->mem = new_mem;
+		write->max_size = next_max_size;
 	}
 
 	if( data_size > 0 ) {
-		memcpy( writer->mem + writer->size, data, data_size );
-		writer->size += data_size;
+		memcpy( write->mem + write->size, data, data_size );
+		write->size += data_size;
 	}
 
 	return( 1 );
@@ -156,7 +244,7 @@ vips_webp_writer_append( VipsWebPWriter *writer,
  */
 #ifdef HAVE_LIBWEBPMUX
 static gboolean
-vips_webp_writer_appendle( VipsWebPWriter *writer, uint32_t val, int n )
+vips_webp_write_appendle( VipsWebPWrite *write, uint32_t val, int n )
 {
 	unsigned char buf[4];
 	int i; 
@@ -168,156 +256,148 @@ vips_webp_writer_appendle( VipsWebPWriter *writer, uint32_t val, int n )
 		val >>= 8;
 	}
 
-	return( vips_webp_writer_append( writer, buf, n ) ); 
+	return( vips_webp_write_append( write, buf, n ) ); 
 }
 
 static gboolean
-vips_webp_writer_appendle32( VipsWebPWriter *writer, uint32_t val )
+vips_webp_write_appendle32( VipsWebPWrite *write, uint32_t val )
 {
-	return( vips_webp_writer_appendle( writer, val, 4 ) ); 
+	return( vips_webp_write_appendle( write, val, 4 ) ); 
 }
 
 static gboolean
-vips_webp_writer_appendle24( VipsWebPWriter *writer, uint32_t val )
+vips_webp_write_appendle24( VipsWebPWrite *write, uint32_t val )
 {
-	return( vips_webp_writer_appendle( writer, val, 3 ) ); 
+	return( vips_webp_write_appendle( write, val, 3 ) ); 
 }
 
 static gboolean
-vips_webp_writer_appendcc( VipsWebPWriter *writer, const char buf[4] )
+vips_webp_write_appendcc( VipsWebPWrite *write, const char buf[4] )
 {
-	return( vips_webp_writer_append( writer, (const uint8_t *) buf, 4 ) ); 
+	return( vips_webp_write_append( write, (const uint8_t *) buf, 4 ) ); 
 }
 
 static gboolean
-vips_webp_writer_appendc( VipsWebPWriter *writer, 
+vips_webp_write_appendc( VipsWebPWrite *write, 
 	const char fourcc[4], const uint8_t *data, guint64 data_size )
 {
 	const int zero = 0;
 	gboolean need_padding = (data_size & 1) != 0;
 
-	if( !vips_webp_writer_appendcc( writer, fourcc ) ||
-		!vips_webp_writer_appendle32( writer, data_size ) ||
-		!vips_webp_writer_append( writer, data, data_size ) )
+	if( !vips_webp_write_appendcc( write, fourcc ) ||
+		!vips_webp_write_appendle32( write, data_size ) ||
+		!vips_webp_write_append( write, data, data_size ) )
 		return( 0 );
 
 	if( need_padding &&
-		!vips_webp_writer_append( writer, (const uint8_t *) &zero, 1 ) )
+		!vips_webp_write_append( write, (const uint8_t *) &zero, 1 ) )
 		return( 0 );
 
 	return( 1 );
 }
 #endif /*HAVE_LIBWEBPMUX*/
 
-static void
-vips_webp_writer_unset( VipsWebPWriter *writer )
+static gboolean
+memory_writer( const uint8_t *data, size_t data_size,
+	const WebPPicture *picture ) 
 {
-	VIPS_FREE( writer->mem );
+	VipsWebPWrite * const write = 
+		(VipsWebPWrite *) picture->custom_ptr;
+
+	if( !write )
+		return( 0 );
+
+	return( vips_webp_write_append( write, data, data_size ) ); 
 }
 
 static gboolean
-memory_write( const uint8_t *data, size_t data_size,
-	const WebPPicture *picture ) 
+vips_webp_pic_init( VipsWebPWrite *write, WebPPicture *pic )
 {
-	VipsWebPWriter * const writer = 
-		(VipsWebPWriter *) picture->custom_ptr;
-
-	if( !writer )
-		return( 0 );
-
-	return( vips_webp_writer_append( writer, data, data_size ) ); 
-}
-
-static int
-write_webp( WebPPicture *pic, VipsImage *in,
-	int Q, gboolean lossless, VipsForeignWebpPreset preset,
-	gboolean smart_subsample, gboolean near_lossless,
-	int alpha_q )
-{
-	VipsImage *memory;
-	WebPConfig config;
-	webp_import import;
-
-	if( !WebPConfigInit( &config ) ) {
-		vips_error( "vips2webp",
-			"%s", _( "config version error" ) );
-		return( -1 );
+	if( !WebPPictureInit( pic ) ) {
+		vips_error( "vips2webp", "%s", _( "picture version error" ) );
+		return( FALSE );
 	}
-
-	/* These presets are only for lossy compression. There seems to be
-	 * separate API for lossless or near-lossless, see
-	 * WebPConfigLosslessPreset().
-	 */
-	if( !(lossless || near_lossless) &&
-		!WebPConfigPreset( &config, get_preset( preset ), Q ) ) {
-		vips_error( "vips2webp", "%s", _( "config version error" ) );
-		return( -1 );
-	}
+	pic->writer = memory_writer;
+	pic->custom_ptr = write;
 
 #if WEBP_ENCODER_ABI_VERSION >= 0x0100
-	config.lossless = lossless || near_lossless;
-	config.alpha_quality = alpha_q;
-	/* Smart subsampling needs use_argb because
-	 * it is applied during RGB to YUV conversion.
+	/* Smart subsampling needs use_argb because it is applied during 
+	 * RGB to YUV conversion.
 	 */
-	pic->use_argb = lossless || near_lossless || smart_subsample;
-#else
-	if( lossless || 
-		near_lossless )
-		g_warning( "%s", _( "lossless unsupported" ) );
-	if( alpha_q != 100 )
-		g_warning( "%s", _( "alpha_q unsupported" ) );
+	pic->use_argb = write->lossless || 
+		write->near_lossless || 
+		write->smart_subsample;
 #endif
 
-#if WEBP_ENCODER_ABI_VERSION >= 0x0209
-	if( near_lossless )
-		config.near_lossless = Q;
-	if( smart_subsample )
-		config.preprocessing |= 4;
-#else
-	if( near_lossless )
-		g_warning( "%s", _( "near_lossless unsupported" ) );
-	if( smart_subsample )
-		g_warning( "%s", _( "smart_subsample unsupported" ) );
-#endif
+	return( TRUE );
+}
 
-	if( !WebPValidateConfig( &config ) ) {
-		vips_error( "vips2webp", "%s", _( "invalid configuration" ) );
-		return( -1 );
-	}
+/* Write a VipsImage into a webp stream.
+ */
+static int
+write_webp( VipsWebPWrite *write, VipsImage *image ) 
+{
+	VipsImage *memory;
+	webp_import import;
+	WebPPicture pic;
 
-	if( !(memory = vips_image_copy_memory( in )) )
+	if( !vips_webp_pic_init( write, &pic ) ) 
 		return( -1 );
 
-	pic->width = memory->Xsize;
-	pic->height = memory->Ysize;
+	if( !(memory = vips_image_copy_memory( image )) )
+		return( -1 );
 
-	if( in->Bands == 4 )
+	pic.width = memory->Xsize;
+	pic.height = memory->Ysize;
+
+	if( memory->Bands == 4 )
 		import = WebPPictureImportRGBA;
 	else
 		import = WebPPictureImportRGB;
 
-	if( !import( pic, VIPS_IMAGE_ADDR( memory, 0, 0 ),
+	if( !import( &pic, VIPS_IMAGE_ADDR( memory, 0, 0 ),
 		VIPS_IMAGE_SIZEOF_LINE( memory ) ) ) {
 		VIPS_UNREF( memory );
+		WebPPictureFree( &pic );
 		vips_error( "vips2webp", "%s", _( "picture memory error" ) );
 		return( -1 );
 	}
 
-	if( !WebPEncode( &config, pic ) ) {
+	if( !WebPEncode( &write->config, &pic ) ) {
 		VIPS_UNREF( memory );
+		WebPPictureFree( &pic );
 		vips_error( "vips2webp", "%s", _( "unable to encode" ) );
 		return( -1 );
 	}
 
 	VIPS_UNREF( memory );
 
+	WebPPictureFree( &pic );
+
 	return( 0 );
 }
 
+#ifdef HAVE_WEBPANIMENCODEROPTIONSINIT
+static int
+write_webp_anim( WebPPicture *pic, VipsImage *in )
+{
+	WebPAnimEncoderOptions anim_config;
+	WebPAnimEncoder *enc;
+
+	if( !WebPAnimEncoderOptionsInit( &anim_config ) ) {
+		vips_error( "vips2webp",
+			"%s", _( "config version error" ) );
+		return( -1 );
+	}
+	enc = NULL;
+
+	return( 0 );
+}
+#endif /*HAVE_WEBPANIMENCODEROPTIONSINIT*/
+
 #ifdef HAVE_LIBWEBPMUX
 static int
-vips_webp_add_chunk( VipsWebPWriter *writer, VipsImage *image, 
+vips_webp_add_chunk( VipsWebPWrite *write, VipsImage *image, 
 	const char *vips, const char webp[4] )
 {
 	if( vips_image_get_typeof( image, vips ) ) {
@@ -328,14 +408,14 @@ vips_webp_add_chunk( VipsWebPWriter *writer, VipsImage *image,
 		 */
 		(void) vips_image_get_blob( image, vips, &data, &length );
 
-		if( !vips_webp_writer_appendc( writer, webp, data, length ) )
+		if( !vips_webp_write_appendc( write, webp, data, length ) )
 			return( -1 ); 
 	}
 
 	return( 0 );
 }
 
-/* Turn @writer into a VP8X image with metadata from @image.
+/* Turn @write into a VP8X image with metadata from @image.
  *
  * Based (partly) on cwep.c
  *
@@ -368,18 +448,20 @@ vips_webp_add_chunk( VipsWebPWriter *writer, VipsImage *image,
  *     https://developers.google.com/speed/webp/docs/riff_container
  */
 static int
-vips_webp_add_metadata( VipsWebPWriter *writer, VipsImage *image )
+vips_webp_add_metadata( VipsWebPWrite *write, VipsImage *image )
 {
-	/* The image in @writer may be VP8X already.
+	/* The image in @write may be VP8X already.
 	 */
-	gboolean is_vp8x = !memcmp( writer->mem + 12, "VP8X", 4 );
-	gboolean is_lossless = !memcmp( writer->mem + 12, "VP8L", 4 );
+	gboolean is_vp8x = !memcmp( write->mem + 12, "VP8X", 4 );
+	gboolean is_lossless = !memcmp( write->mem + 12, "VP8L", 4 );
 
 	guint64 metadata_size;
 	uint32_t flags;
 	int i;
+
 	guint64 new_size;
-	VipsWebPWriter new;
+	uint8_t *old_mem;
+	guint64 old_size;
 
 	/* Rebuild the EXIF block, if any, ready for writing. 
 	 */
@@ -393,7 +475,7 @@ vips_webp_add_metadata( VipsWebPWriter *writer, VipsImage *image )
 
 	/* If there are any flags there already, we add to them.
 	 */
-	flags = is_vp8x ? writer->mem[20] : 0;
+	flags = is_vp8x ? write->mem[20] : 0;
 
 	for( i = 0; i < vips__n_webp_names; i++ ) { 
 		const char *vips = vips__webp_names[i].vips;
@@ -425,27 +507,39 @@ vips_webp_add_metadata( VipsWebPWriter *writer, VipsImage *image )
 	/* If it's not already vp8x, we'll need to add a vp8x header, and
 	 * that'll add 18 bytes. -8 since size includes the RIFF header.
 	 */
-	new_size = writer->size - 8 + (is_vp8x ? 0 : 18) + metadata_size;
+	new_size = write->size - 8 + (is_vp8x ? 0 : 18) + metadata_size;
 
-	vips_webp_writer_init( &new );
+	/* This is really terrible.
+	 *
+	 * Note the old memory buffer and reset writer to be empty. We write
+	 * the entire image again, pasting bits together from the previous
+	 * write.
+	 */
 
-	if( !vips_webp_writer_appendcc( &new, "RIFF" ) ||
-		!vips_webp_writer_appendle32( &new, new_size ) ||
-		!vips_webp_writer_appendcc( &new, "WEBP" ) ||
-		!vips_webp_writer_appendcc( &new, "VP8X" ) ||
-		!vips_webp_writer_appendle32( &new, 10 ) ) { 
-		vips_webp_writer_unset( &new );
+	old_size = write->size;
+	old_mem = write->mem;
+
+	write->mem = NULL;
+	write->size = 0;
+	write->max_size = 0;
+
+	if( !vips_webp_write_appendcc( write, "RIFF" ) ||
+		!vips_webp_write_appendle32( write, new_size ) ||
+		!vips_webp_write_appendcc( write, "WEBP" ) ||
+		!vips_webp_write_appendcc( write, "VP8X" ) ||
+		!vips_webp_write_appendle32( write, 10 ) ) { 
+		VIPS_FREE( old_mem );
 		return( -1 );
 	}
 
 	if( is_vp8x ) {
 		/* Copy the existing VP8X body and update the flag bits.
 		 */
-		if( !vips_webp_writer_append( &new, writer->mem + 20, 10 ) ) {
-			vips_webp_writer_unset( &new );
+		if( !vips_webp_write_append( write, old_mem + 20, 10 ) ) {
+			VIPS_FREE( old_mem );
 			return( -1 );
 		}
-		new.mem[20] = flags;
+		write->mem[20] = flags;
 	}
 	else {
 		/* We have to make a new vp8x header.
@@ -458,55 +552,52 @@ vips_webp_add_metadata( VipsWebPWriter *writer, VipsImage *image )
 		 * another 4 to skip the length, then bit 8 - 3 == 5
 		 */
 		if( is_lossless &&
-			(writer->mem[12 + 8 + 3] & (1 << 5)) )
+			(old_mem[12 + 8 + 3] & (1 << 5)) )
 			flags |= 0x010;
 
 		/* 10 is the length of the VPX8X header chunk.
 		 */
-		if( !vips_webp_writer_appendle32( &new, flags ) ||
-			!vips_webp_writer_appendle24( &new, image->Xsize - 1 ) ||
-			!vips_webp_writer_appendle24( &new, image->Ysize- 1 ) ) {
-			vips_webp_writer_unset( &new );
+		if( !vips_webp_write_appendle32( write, flags ) ||
+			!vips_webp_write_appendle24( write, 
+				image->Xsize - 1 ) ||
+			!vips_webp_write_appendle24( write, 
+				image->Ysize - 1 ) ) {
+			VIPS_FREE( old_mem );
 			return( -1 );
 		}
 	}
 
 	/* Extra chunks have to be in this order.
 	 */
-	if( vips_webp_add_chunk( &new, image, VIPS_META_ICC_NAME, "ICCP" ) ) {
-		vips_webp_writer_unset( &new );
+	if( vips_webp_add_chunk( write, image, VIPS_META_ICC_NAME, "ICCP" ) ) {
+		VIPS_FREE( old_mem );
 		return( -1 );
 	}
 
 	/* The image chunk must come here.
 	 */
 	if( is_vp8x ) {
-		if( !vips_webp_writer_append( &new, 
-			writer->mem + 30, writer->size - 30 ) ) {
-			vips_webp_writer_unset( &new );
+		if( !vips_webp_write_append( write, 
+			old_mem + 30, old_size - 30 ) ) {
+			VIPS_FREE( old_mem );
 			return( -1 );
 		}
 	}
 	else {
-		if( !vips_webp_writer_append( &new, 
-			writer->mem + 12, writer->size - 12 ) ) {
-			vips_webp_writer_unset( &new );
+		if( !vips_webp_write_append( write, 
+			old_mem + 12, old_size - 12 ) ) {
+			VIPS_FREE( old_mem );
 			return( -1 );
 		}
 	}
 
-	if( vips_webp_add_chunk( &new, image, VIPS_META_EXIF_NAME, "EXIF" ) ) {
-		vips_webp_writer_unset( &new );
-		return( -1 );
-	}
+	VIPS_FREE( old_mem );
 
-	if( vips_webp_add_chunk( &new, image, VIPS_META_XMP_NAME, "XMP " ) ) {
-		vips_webp_writer_unset( &new );
+	if( vips_webp_add_chunk( write, image, VIPS_META_EXIF_NAME, "EXIF" ) ) 
 		return( -1 );
-	}
 
-	vips_webp_writer_unset( writer );
-	*writer = new;
+	if( vips_webp_add_chunk( write, image, VIPS_META_XMP_NAME, "XMP " ) ) 
+		return( -1 );
 
 	return( 0 );
 }
@@ -518,50 +609,41 @@ vips__webp_write_file( VipsImage *in, const char *filename,
 	gboolean smart_subsample, gboolean near_lossless,
 	int alpha_q, gboolean strip )
 {
-	WebPPicture pic;
-	VipsWebPWriter writer;
+	VipsWebPWrite write;
 	FILE *fp;
 
-	if( !WebPPictureInit( &pic ) ) {
-		vips_error( "vips2webp",
-			"%s", _( "picture version error" ) );
+	if( vips_webp_write_init( &write, in,
+		Q, lossless, preset, smart_subsample, near_lossless,
+		alpha_q, strip ) )
+		return( -1 );
+
+	if( write_webp( &write, in ) ) { 
+		vips_webp_write_unset( &write );
 		return( -1 );
 	}
-
-	vips_webp_writer_init( &writer );
-	pic.writer = memory_write;
-	pic.custom_ptr = &writer;
-
-	if( write_webp( &pic, in, Q, lossless, preset, smart_subsample,
-		near_lossless, alpha_q ) ) {
-		WebPPictureFree( &pic );
-		vips_webp_writer_unset( &writer );
-		return( -1 );
-	}
-
-	WebPPictureFree( &pic );
 
 #ifdef HAVE_LIBWEBPMUX
 	if( !strip &&
-		vips_webp_add_metadata( &writer, in ) ) {
-		vips_webp_writer_unset( &writer );
+		vips_webp_add_metadata( &write, in ) ) {
+		vips_webp_write_unset( &write );
 		return( -1 );
 	}
 #endif /*HAVE_LIBWEBPMUX*/
 
 	if( !(fp = vips__file_open_write( filename, FALSE )) ) {
-		vips_webp_writer_unset( &writer );
+		vips_webp_write_unset( &write );
 		return( -1 );
 	}
 
-	if( vips__file_write( writer.mem, writer.size, 1, fp ) ) {
+	if( vips__file_write( write.mem, write.size, 1, fp ) ) {
 		fclose( fp );
-		vips_webp_writer_unset( &writer );
+		vips_webp_write_unset( &write );
 		return( -1 );
 	}
 
 	fclose( fp );
-	vips_webp_writer_unset( &writer );
+
+	vips_webp_write_unset( &write );
 
 	return( 0 );
 }
@@ -572,38 +654,31 @@ vips__webp_write_buffer( VipsImage *in, void **obuf, size_t *olen,
 	gboolean smart_subsample, gboolean near_lossless,
 	int alpha_q, gboolean strip )
 {
-	WebPPicture pic;
-	VipsWebPWriter writer;
+	VipsWebPWrite write;
 
-	if( !WebPPictureInit( &pic ) ) {
-		vips_error( "vips2webp", 
-			"%s", _( "picture version error" ) );
+	if( vips_webp_write_init( &write, in,
+		Q, lossless, preset, smart_subsample, near_lossless,
+		alpha_q, strip ) )
+		return( -1 );
+
+	if( write_webp( &write, in ) ) {
+		vips_webp_write_unset( &write );
 		return( -1 );
 	}
-
-	vips_webp_writer_init( &writer );
-	pic.writer = memory_write;
-	pic.custom_ptr = &writer;
-
-	if( write_webp( &pic, in, Q, lossless, preset, smart_subsample,
-		near_lossless, alpha_q ) ) {
-		WebPPictureFree( &pic );
-		vips_webp_writer_unset( &writer );
-		return( -1 );
-	}
-
-	WebPPictureFree( &pic );
 
 #ifdef HAVE_LIBWEBPMUX
 	if( !strip &&
-		vips_webp_add_metadata( &writer, in ) ) {
-		vips_webp_writer_unset( &writer );
+		vips_webp_add_metadata( &write, in ) ) {
+		vips_webp_write_unset( &write );
 		return( -1 );
 	}
 #endif /*HAVE_LIBWEBPMUX*/
 
-	*obuf = writer.mem;
-	*olen = writer.size;
+	*obuf = write.mem;
+	write.mem = NULL;
+	*olen = write.size;
+
+	vips_webp_write_unset( &write );
 
 	return( 0 );
 }
